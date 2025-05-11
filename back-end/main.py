6781545b22a8ecd88f2c38f39cd055959943a11c
Alex Andrade <83988053+alexandrade-git@users.noreply.github.com>
2025-05-11 00:19:05 -0300

    Adicionado letra e

--- back-end/main.py
+++ back-end/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     expose_headers=["*"]
 )
 
-# Endpoint de teste
+# Endpoint de testee
 @app.get("/test")
 async def test_endpoint():
     logger.info("Test endpoint called")
